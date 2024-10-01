6cd505d3dffc0a5ccc9a99191a7984338ee7305b
Felipe Marques <fm043445@gmail.com>
2024-10-01 11:00:47 -0300

    Fazendo o consumo de uma API de noticias do IBGE

---
+++ ibge-noticias.py
@@ -0,0 +1,18 @@
+
+import requests
+
+try:
+    response = requests.get('http://servicodados.ibge.gov.br/api/v3/noticias/?qtd=10')
+
+    if response.status_code == 200:
+        dados = response.json()
+        for item in dados['items']:
+            print(f"Titulo: {item['titulo']}")
+            print(f"Resumo: {item['introducao']}")
+            print('-'* 40)
+    else:
+        print('Erro de requisição')
+except requests.exceptions.ConnectionError:
+    print('Erro de Requisição')
+except Exception:
+    print('Erro.')
